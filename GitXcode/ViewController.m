a4c7a6ba8f55c4f33ad59ca4efceb1dc05a16e48
Student <student@achiever130.local>
2017-01-02 00:23:50 -0800

    Added nslog statement

--- GitXcode/ViewController.m
+++ GitXcode/ViewController.m
@@ -16,6 +16,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    NSLog(@"This my first project under version controller");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
